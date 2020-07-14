ecb0d070848b85f847e80b0703d38882c378015d
jharteaga <jh.arteaga09@gmail.com>
2020-07-14 00:41:54 -0700

    Add SearchBar Component

---
+++ app/components/SearchBar.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import colors from '../config/colors';
+
+function SearchBar(props) {
+  const [city, setCity] = useState('');
+
+  const handleChange = (text) => {
+    setCity(text);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={city === '' ? styles.placeholder : styles.label}>City</Text>
+      <View style={styles.row}>
+        <TextInput
+          style={styles.text}
+          onChangeText={(text) => handleChange(text)}
+        >
+          {city}
+        </TextInput>
+        <TouchableOpacity>
+          <MaterialCommunityIcons
+            name="tune"
+            color={colors.secondary}
+            size={30}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderBottomWidth: 3,
+    borderColor: colors.lightGray,
+    paddingBottom: 10,
+    width: '100%',
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  placeholder: {
+    color: colors.secondary,
+    fontSize: 40,
+    letterSpacing: 1,
+    position: 'relative',
+    top: 50,
+  },
+  label: {
+    color: colors.secondary,
+    fontSize: 18,
+    letterSpacing: 1,
+    position: 'relative',
+    top: -10,
+  },
+  row: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
+
+export default SearchBar;

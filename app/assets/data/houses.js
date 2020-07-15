d528033c2fe315a826298d2f4fa6edee254a6e2b
jharteaga <jh.arteaga09@gmail.com>
2020-07-14 23:47:39 -0700

    Add house data to list

---
+++ app/assets/data/houses.js
@@ -0,0 +1,20 @@
+const houses = [
+  {
+    id: 1,
+    imageUrl:
+      'https://res.cloudinary.com/di5y8jhl0/image/upload/v1594794357/rn-real-state/home11_dw1ard.jpg',
+    thumbnailUrl:
+      'https://res.cloudinary.com/di5y8jhl0/image/upload/c_thumb,w_100/v1594794357/rn-real-state/home11_dw1ard.jpg',
+    price: '$200,000',
+    city: 'San Francisco',
+    address: '1225 Robson St, 94080, SF',
+    description:
+      'Clean & well kept 2480 SQFT two-level home on a large 46.5x120 lot on a quiet cul-de-sac & high side of the road in popular Fraserview area! Featuring 3 bedrooms & 2 Baths on the top floor, bright & spacious living room & dining room with laminate flooring and new paint throughout & updated kitchen with newer appliances & Fischer Paykal Induction Stove. ',
+    squareFeet: 2408,
+    bedrooms: 2,
+    bathrooms: 2,
+    garage: 2,
+    publishedDate: '2020-07-13',
+    location: { latitude: 37.78719, longitude: -122.408421 },
+  },
+];
